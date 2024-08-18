4703f48bbcdd08728ac9805c8b575a8b54aaabd8
israel.joseph <israel-joseph@hotmail.com>
2024-08-18 20:27:04 -0300

    Arquivos básicos de configuração da API

---
+++ backend/src/main/java/com/israeljoseph/Task/management/TaskManagementApplication.java
@@ -0,0 +1,12 @@
+package com.israeljoseph.Task.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApplication.class, args);
+	}
+}
